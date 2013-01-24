713be04bfb82ddb538bcb61d774fb2b5e83845c0
ROM CH3F/Lens_Flare <tnoah12@gmail.com>
2013-01-24 04:43:13 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,7 @@ add_lunch_combo pa_grouper-userdebug
 add_lunch_combo pa_i9100-userdebug
 add_lunch_combo pa_i9300-userdebug
 add_lunch_combo pa_tilapia-userdebug
+add_lunch_combo pa_jewel-userdebug
 add_lunch_combo pa_maguro-userdebug
 add_lunch_combo pa_mako-userdebug
 add_lunch_combo pa_manta-userdebug
